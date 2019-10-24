a2f14adcf120a33efbd8230934c3f301aff2ab77
Moxank6277 <42675676+Moxank6277@users.noreply.github.com>
2019-10-24 16:56:14 +0530

    l

--- src/app/textbook/textbook.component.ts
+++ src/app/textbook/textbook.component.ts
@@ -15,32 +15,32 @@ export class TextbookComponent implements OnInit {
   public uname: string;
   public password: string;
   public email: string;
-  public obj:any;
-  public loggedIN=false;
+  public obj: any;
+  public loggedIN = false;
 
-  public pageCourse:string;
+  public pageCourse: string;
 
-  public clist:UserCategory[];
+  public clist: UserCategory[];
 
-  public textBooks:Textbook[]=[];
-  public selectedTextbook:Textbook[]=[];
+  public textBooks: Textbook[] = [];
+  public selectedTextbook: Textbook[] = [];
 
-  public noTextbook:boolean=false;
+  public noTextbook: boolean = false;
 
-  public toogleTextbook:boolean=false;
+  public toogleTextbook: boolean = false;
 
-  public filePath:string;
+  public filePath: string;
 
-  public tempFlag:Boolean=false;
+  public tempFlag: Boolean = false;
   constructor(private DB: DataBaseService, private Cookie: CookieService, private router: Router) { }
 
-  ngOnDestroy(){
-    $("body").css({ background: 'none'});
+  ngOnDestroy() {
+    $("body").css({ background: 'none' });
   }
 
   ngOnInit() {
     $("#contentBack").remove();
-   // $("body").css({ background: 'linear-gradient(to right,#83a4d4, #b6fbff)'});
+    // $("body").css({ background: 'linear-gradient(to right,#83a4d4, #b6fbff)'});
 
     var x = this.Cookie.get('LoggedIN');
     if (x == 'true') {
@@ -54,34 +54,33 @@ export class TextbookComponent implements OnInit {
       this.router.navigate(['/Home']);
     }
     //console.log(this.obj.email);
-    this.DB.getUserCategories(this.obj.email).subscribe(data=>{
+    this.DB.getUserCategories(this.obj.email).subscribe(data => {
       this.clist = data;
     });
 
-    this.DB.getEveryBook().subscribe(data=>{
-      this.textBooks=data.records;
-      if(this.textBooks.length==0)
-        this.noTextbook=true;
-      
+    this.DB.getEveryBook().subscribe(data => {
+      this.textBooks = data.records;
+      if (this.textBooks.length == 0)
+        this.noTextbook = true;
+
     });
   }
-  updatePage(){
-    this.tempFlag=true;
-    this.noTextbook=false;
-    this.selectedTextbook=[];
-    this.textBooks.forEach(book=>{
-      if(book.Course==this.pageCourse){
+  updatePage() {
+    this.tempFlag = true;
+    this.noTextbook = false;
+    this.selectedTextbook = [];
+    this.textBooks.forEach(book => {
+      if (book.Course == this.pageCourse) {
         this.selectedTextbook.push(book);
-        }
+      }
     });
-    if(this.selectedTextbook.length==0){
-      this.noTextbook=true;
-      this.toogleTextbook=false;
+    if (this.selectedTextbook.length == 0) {
+      this.noTextbook = true;
+      this.toogleTextbook = false;
     }
-  } 
-  public show(path:string)
-  {
-    this.toogleTextbook=true;
-    this.filePath=path;
+  }
+  public show(path: string) {
+    this.toogleTextbook = true;
+    this.filePath = path;
   }
 }
